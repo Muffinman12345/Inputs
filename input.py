37a33f225fc472daafe199a7d3f75191c4d7085b
ddfunk2 <77017235+ddfunk2@users.noreply.github.com>
2021-02-04 11:12:24 -0500

    Done

---
+++ input.py
@@ -0,0 +1,12 @@
+#An input is simple. 
+
+input("Enter your name: ")
+
+#THis will ask the user to enter their name. But after this is over, the data is not saved! We can make a variable into an input:
+
+Num = input("Enter a number: ")
+
+#Sometimes, we want just an integer (like the example above. The user could enter a k!
+#So, we can make an input an integer:
+
+Num = int(input("Enter a number: "))
